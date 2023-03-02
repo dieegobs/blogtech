43d50d313d8bfc97bf0f19ff1b8da6ea096576f6
dieegobs <diegohevant@gmail.com>
2023-03-01 23:22:28 -0300

    Finalizando parte do post

--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function CardPost(){
     return (
-        <div className="w-full max-w-[352px] h-full flex flex-col items-center justify-between gap-16">
+        <div className="w-full max-w-[352px] h-full flex flex-col items-center justify-between gap-7">
             <div className='flex w-full h-[234px] relative rounded-2xl overflow-hidden'>
             <Image
               src="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
